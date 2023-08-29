af524c83ec03c3a089ba00a890cab33e3bdf01a9
Daniel Monettelli <danielmonetelli@hotmail.com>
2023-08-29 02:28:34 -0500

    🚀 [Code] Updated Meow c8

---
+++ Meow/Controls/CustomStringSplitterView.cs
@@ -0,0 +1,77 @@
+﻿namespace Meow.Controls;
+
+public class CustomStringSplitterView : ContentView
+{
+    private StackLayout itemsLayout;
+
+    public static readonly BindableProperty InputTextProperty =
+        BindableProperty.Create(nameof(InputText), typeof(string), typeof(CustomStringSplitterView), null, propertyChanged: OnInputTextChanged);
+
+    public string InputText
+    {
+        get { return (string)GetValue(InputTextProperty); }
+        set { SetValue(InputTextProperty, value); }
+    }
+
+    public CustomStringSplitterView()
+    {
+        itemsLayout = new StackLayout
+        {
+            Spacing = 6,
+            Orientation = StackOrientation.Horizontal
+        };
+
+        Content = new ScrollView
+        {
+            HorizontalScrollBarVisibility = ScrollBarVisibility.Never,
+            Orientation = ScrollOrientation.Horizontal,
+            Content = itemsLayout
+        };
+    }
+
+    private static void OnInputTextChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is CustomStringSplitterView splitter)
+        {
+            splitter.UpdateItems();
+        }
+    }
+
+    private void UpdateItems()
+    {
+        itemsLayout.Children.Clear();
+
+        if (!string.IsNullOrWhiteSpace(InputText))
+        {
+            string cleanedText = InputText.Replace(",", "");
+            string[] items = cleanedText.Split(' ');
+
+            foreach (string item in items)
+            {
+                Label label = new()
+                {
+                    Text = item,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center
+
+                };
+                label.SetAppThemeColor(Label.TextColorProperty, Color.FromArgb("#6d1995"), Color.FromArgb("#a183e1"));
+
+                Border border = new()
+                {
+                    Content = label,
+                    Padding = new Thickness(8, 6, 8, 6),
+                    Background = Colors.Transparent,
+                    StrokeShape = new RoundRectangle
+                    {
+                        CornerRadius = new CornerRadius(7, 7, 7, 7)
+                    },
+                    StrokeThickness = 1,
+                };
+                border.SetAppThemeColor(Border.StrokeProperty, Color.FromArgb("#6d1995"), Color.FromArgb("#a183e1"));
+
+                itemsLayout.Children.Add(border);
+            }
+        }
+    }
+}
--- Meow/GlobalUsings.cs
+++ Meow/GlobalUsings.cs
@@ -9,6 +9,7 @@ global using Meow.Services;
 global using Meow.ViewModels;
 global using Meow.Views;
 global using Microsoft.Extensions.Logging;
+global using Microsoft.Maui.Controls.Shapes;
 global using System.Net.Http.Headers;
 global using System.Text;
 global using System.Text.Json;
