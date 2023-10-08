e563984689498e700256dab4ac0c06074173dc10
Daniel Monettelli <danielmonetelli@hotmail.com>
2023-10-07 23:40:39 -0500

    🚀 [Code] Updated Meow s8

--- Meow/Constants/APIConstants.cs
+++ Meow/Constants/APIConstants.cs
@@ -3,7 +3,7 @@ namespace Meow.Constants;
 public static class APIConstants
 {
     public const string APIBaseUrl = "https://api.thecatapi.com/v1/";
-    public const string APIKey = "live_HzF8UyYCwcjqY05axorP4F5UFp20fewj8xV5aNX8RTpPkd078qsX7umR5Cqy85qw";
+    public const string APIKey = "THECAT_API_KEY_HERE";
 
     public const string CustomRandomKittyEndPoint = "images/search?size=med&mime_types=jpg,png&has_breeds=true&include_breeds=true&include_categories=true";
     public const string CustomRandomKittiesByBreedEndPoints = "images/search?size=med&mime_types=jpg,png&limit=10&has_breeds=true&include_breeds=true&include_categories=true";
